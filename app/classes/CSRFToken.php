1de9d9a2a2bb73c7e6504fbcb4e4ed3d9f6684b2
Brett Hartman <brett.hartman@mac.com>
2018-12-22 10:43:02 -0800

    Create CSRFToken class

---
+++ app/classes/CSRFToken.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace app\classes;
+
+class CSRFToken
+{
+	public static function _token(): string
+	{
+		if (!Session::has('token')) {
+			/** @noinspection PhpUnhandledExceptionInspection */
+			$randomToken = bin2hex(random_bytes(32));
+
+			Session::add('token', $randomToken);
+		}
+
+		return Session::get('token');
+	}
+
+	public static function verifyCSRFToken($requestToken): bool
+	{
+		return Session::has('token') && Session::get('token') === $requestToken;
+	}
+}
