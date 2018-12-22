3f132d583d1faf7556c14354ae97ed55c3b4d1b7
Brett Hartman <brett.hartman@mac.com>
2018-12-22 13:40:33 -0800

    Complete UploadImage class

--- app/classes/UploadImage.php
+++ app/classes/UploadImage.php
@@ -2,6 +2,8 @@
 
 namespace app\classes;
 
+use RuntimeException;
+
 class UploadImage
 {
 	private static $maxFilesize = 2097152;
@@ -26,22 +28,45 @@ class UploadImage
 		return true;
 	}
 
-	public function getFilename()
-	{
-		return $this->filename;
-	}
-
-	public function getPath()
-	{
-		return $this->path;
-	}
-
 	private static function checkFileExtension($file)
 	{
 		return pathinfo($file, PATHINFO_EXTENSION);
 	}
 
-	private function setName($file, $name = null): void
+	public function move($tempPath, $folder, $file, $newFilename)
+	{
+		$ds = DIRECTORY_SEPARATOR;
+
+		$this->setFilename($file, $newFilename);
+
+		$filename = $this->getFilename();
+
+		if (!is_dir($folder) && !mkdir($folder, 0775, true) && !is_dir($folder)) {
+			throw new RuntimeException("Directory $folder was not created successfully.");
+		}
+
+		$this->setPath("{$folder}{$ds}{$filename}");
+
+		$absolutePath = BASE_PATH . "{$ds}public{$ds}" . $this->getPath();
+
+		if (move_uploaded_file($tempPath, $absolutePath)) {
+			return $this->getPath();
+		}
+
+		return null;
+	}
+
+	private function getFilename()
+	{
+		return $this->filename;
+	}
+
+	private function getPath()
+	{
+		return $this->path;
+	}
+
+	private function setFilename($file, $name = null): void
 	{
 		if (!isset($name) || $name === '') {
 			$name = pathinfo($file, PATHINFO_FILENAME);
@@ -52,5 +77,10 @@ class UploadImage
 		$ext = self::checkFileExtension($file);
 
 		$this->filename = "{$name}-{$hash}.{$ext}";
+	}
+
+	private function setPath($path): void
+	{
+		$this->path = $path;
 	}
 }
